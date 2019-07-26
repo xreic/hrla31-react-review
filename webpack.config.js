901772f289dd359b58564d422923e6072b10e956
Matthew Mata <matthewmata1030@gmail.com>
2019-07-25 19:39:32 -0700

    [init] setup inital folder structure and server

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+const SRC_DIR = path.join(__dirname, 'client/src');
+const DIST_DIR = path.join(__dirname, 'client/dist');
+
+module.exports = {
+  mode: 'development',
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react']
+        }
+      }
+    ]
+  }
+}
