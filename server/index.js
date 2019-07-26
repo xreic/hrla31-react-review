7a1278ab7c4c1c9b2547ae2435bedb0c670f50e2
Matthew Mata <matthewmata1030@gmail.com>
2019-07-25 20:12:24 -0700

    [init] changed folder structure

--- server/index.js
+++ server/index.js
@@ -4,6 +4,6 @@ const path = require('path');
 const app = express()
 const port = 3000
 
-app.use(express.static(path.resolve(__dirname, '../client/dist')));
+app.use(express.static(path.resolve(__dirname, '../static')));
 
 app.listen(port, () => console.log(`Todo List listening on port ${port}!`))
