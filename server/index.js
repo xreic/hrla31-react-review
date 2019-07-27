2a50cbb4932883b503cf69a3e084bd58e23cd546
Matthew Mata <matthewmata1030@gmail.com>
2019-07-27 11:51:27 -0700

    [init] fixed server and webpack

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
-const express = require('express')
+const express = require('express');
 const path = require('path');
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 app.use(express.static(path.resolve(__dirname, '../static')));
 
